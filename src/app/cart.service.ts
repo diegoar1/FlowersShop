b1bf55f54fba2a843d91deadf2c0f3b8e3eb5b2b
frruvalcaba <fruvalcaba@ozelot.it>
2019-03-11 00:35:00 -0500

    Categorias

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -7,24 +7,44 @@ export class CartService {
  
   private data = [
     {
+      category: 'Tulipanes',
+      expanded: true,
+      products: [
+        { id: 0, name: 'Amarillos', price: '8' },
+        { id: 1, name: 'Rosas', price: '5' },
+        { id: 2, name: 'Blancos', price: '9' },
+        { id: 3, name: 'Rosas', price: '7' }
+      ]
+    },
+    {
       category: 'Rosas',
       expanded: true,
       products: [
         { id: 0, name: 'Rojas', price: '8' },
         { id: 1, name: 'Blancas', price: '5' },
         { id: 2, name: 'Amarillas', price: '9' },
-        { id: 3, name: 'Rosas', price: '7' }
+        { id: 3, name: 'Rosas', price: '7' },
+        { id: 4, name: 'Violeta', price: '7' },
+
       ]
     },
     {
-      category: 'Pasta',
+      category: 'Claveles',
       products: [
         { id: 4, name: 'Mac & Cheese', price: '8' },
         { id: 5, name: 'Bolognese', price: '6' }
       ]
     },
     {
-      category: 'Salad',
+      category: 'Lirios',
+      products: [
+        { id: 6, name: 'Ham & Egg', price: '8' },
+        { id: 7, name: 'Basic', price: '5' },
+        { id: 8, name: 'Ceaser', price: '9' }
+      ]
+    },
+    {
+      category: 'Orquídeas',
       products: [
         { id: 6, name: 'Ham & Egg', price: '8' },
         { id: 7, name: 'Basic', price: '5' },
